98eee9ccbf1468d61ce1a63c072ff920d1039bfc
Esteban <estebanguzra@gmail.com>
2020-06-26 14:18:04 -0600

    delete coments

--- src/main/java/view/GaltonBoard.java
+++ src/main/java/view/GaltonBoard.java
@@ -20,23 +20,7 @@ public class GaltonBoard extends JPanel{
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
         drawDots(g);
-        //setCoordinates();
-//        for (int i = 0; i < list.size(); i++) {
-//            g2.draw(list.get(i).getDrawable().getBounds2D());
-//            if (list.get(i).getBinomialCoefficient() > 99) {
-//                g.drawString(list.get(i).getBinomialCoefficient() + "",
-//                    (int) list.get(i).getDrawable().getBounds2D().getCenterX() - 10,
-//                    (int) list.get(i).getDrawable().getBounds2D().getCenterY() + 2);
-//            } else if (list.get(i).getBinomialCoefficient() > 9 ) {
-//                g.drawString(list.get(i).getBinomialCoefficient() + "",
-//                    (int) list.get(i).getDrawable().getBounds2D().getCenterX() - 6,
-//                    (int) list.get(i).getDrawable().getBounds2D().getCenterY() + 2);
-//            } else {
-//                g.drawString(list.get(i).getBinomialCoefficient() + "",
-//                        (int) list.get(i).getDrawable().getBounds2D().getCenterX() - 2,
-//                        (int) list.get(i).getDrawable().getBounds2D().getCenterY() + 2);
-//            }
-//        }
+        
     }
     private void drawDots(Graphics g) {
         int y = 30;
@@ -49,7 +33,6 @@ public class GaltonBoard extends JPanel{
         for (int row = 1; row < rows + 1; row++) {
             for (int celd = 0; celd < row; celd++) {
                 g.drawOval(x, y, 5, 5);
-                System.out.println("Row:" + row + "/count:" + count);
                 if (celd == row-1) {
                     x -= ((distance) * row) - (distance / 2);
                     y += distance;
